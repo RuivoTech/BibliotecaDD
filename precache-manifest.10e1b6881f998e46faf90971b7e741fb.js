46dea1c7cff4e10de5649c3c2f88263f299f47b8
Richieri Negri <ruivotech@hotmail.com>
2020-08-13 15:09:44 -0300

    Updates

---
+++ precache-manifest.10e1b6881f998e46faf90971b7e741fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473b3e54db0cd7ae0a540781d0f1394f",
+    "url": "/BibliotecaDD/index.html"
+  },
+  {
+    "revision": "001d23b6e49587939a2a",
+    "url": "/BibliotecaDD/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "91e3efba77447eac139c",
+    "url": "/BibliotecaDD/static/css/main.e42aae9e.chunk.css"
+  },
+  {
+    "revision": "001d23b6e49587939a2a",
+    "url": "/BibliotecaDD/static/js/2.5745190b.chunk.js"
+  },
+  {
+    "revision": "54290d94de474a204cee26e039f2fb48",
+    "url": "/BibliotecaDD/static/js/2.5745190b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e3efba77447eac139c",
+    "url": "/BibliotecaDD/static/js/main.66aa869b.chunk.js"
+  },
+  {
+    "revision": "71c6267bf60840be167d",
+    "url": "/BibliotecaDD/static/js/runtime-main.237f7836.js"
+  },
+  {
+    "revision": "f4293a16ba6dbab2506f96f42f7c3df1",
+    "url": "/BibliotecaDD/static/media/Imagem.f4293a16.jpg"
+  },
+  {
+    "revision": "ed330dc2604ff21d30662085c5f49bbb",
+    "url": "/BibliotecaDD/static/media/Logo.ed330dc2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35ba34db6dd16df12c5e32a0507785f7.js"
+  "/BibliotecaDD/precache-manifest.10e1b6881f998e46faf90971b7e741fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BibliotecaDD/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
