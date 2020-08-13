fdbe09630fa30ba92bf1f4c6f4149e8908ae2e15
Richieri Negri <ruivotech@hotmail.com>
2020-08-13 15:45:07 -0300

    Updates

---
+++ precache-manifest.3e9b5343f33ea454b2b7255b9b7c4d8b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13547ece779772a94d5ecb86947b4f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da128c4ecd3cec6ffa72",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "79bf5cbacb08a49863ec",
+    "url": "/static/css/main.e42aae9e.chunk.css"
+  },
+  {
+    "revision": "da128c4ecd3cec6ffa72",
+    "url": "/static/js/2.1e945d86.chunk.js"
+  },
+  {
+    "revision": "54290d94de474a204cee26e039f2fb48",
+    "url": "/static/js/2.1e945d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79bf5cbacb08a49863ec",
+    "url": "/static/js/main.2b9a9de8.chunk.js"
+  },
+  {
+    "revision": "07072aff5a9c77f843f0",
+    "url": "/static/js/runtime-main.5e43deee.js"
+  },
+  {
+    "revision": "f4293a16ba6dbab2506f96f42f7c3df1",
+    "url": "/static/media/Imagem.f4293a16.jpg"
+  },
+  {
+    "revision": "ed330dc2604ff21d30662085c5f49bbb",
+    "url": "/static/media/Logo.ed330dc2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BibliotecaDD/precache-manifest.10e1b6881f998e46faf90971b7e741fb.js"
+  "/precache-manifest.3e9b5343f33ea454b2b7255b9b7c4d8b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BibliotecaDD/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
